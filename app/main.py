789ffbc474dbf251a89da6e884ee524ca50ec4f0
Regis Ximendes <regis.ximendes@ifood.com.br>
2021-08-30 23:19:42 -0300

    remoce words connections/usr/local/opt/python@3.9/bin/python3.9

--- app/main.py
+++ app/main.py
@@ -1,29 +1,31 @@
 import unidecode
 
 def open_source_text():
-    with open('text/source_text.txt', encoding='utf-8', mode="r") as f:
-        text = f.read()
-        f.close()
+    #with open('source_text.txt', encoding='utf-8', mode="r") as f:
+    with open("/Users/regisdarosa/PycharmProjects/keyword-sorter/app/text/source_text.txt", encoding='utf-8', mode="r") as file:
+        text = file.read()
+        file.close()
     return text
 
 def generate_terms(text):
-    decoded_text = unidecode.unidecode(text.upper())
-    terms = strip_text(decoded_text.split(" "))
-    
-    return terms
+    decoded_text = unidecode.unidecode(text)
+    return decoded_text.lower().split(" ")
 
-def strip_text(terms):
-    with open('collections/filter.txt', encoding='utf-8', mode="r") as f:
-        filters = f.read()
-        f.close()
+def remove_word_connections_from_terms(terms):
+    terms_without_connections_list = []
+    words_to_remove = ["para","o","os","a","as","um","uns","uma","umas","a","ao","aos","de","do","dos","da","das","dum","duns","duma","dumas","em","no","nos","na","nas","num","nuns","numa","numas","por","per","pelo","pelos","pela","pelas","e","ou","entao","senao"]
+    for term in terms:
+        if term not in words_to_remove:
+            terms_without_connections_list.append(term)
+    return terms_without_connections_list
 
-    filters = filters.upper()
-    filters = filters.split(",")
-
-    for filter in filters:
-        terms = [word for word in terms if word != filter]
-    return terms
+def calc(terms_without_connections):
+    textSize = len(terms_without_connections)
+   
+    return ""        
 
 text = open_source_text()
 terms = generate_terms(text)
-print(terms)
+terms_without_connections = remove_word_connections_from_terms(terms)
+
+print(terms_without_connections)
